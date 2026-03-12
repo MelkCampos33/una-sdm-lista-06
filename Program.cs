8bdf271f135cd28c2302efe33b3e3336baa21078
Melk <melquisedequecampos.trabalho@gmail.com>
2026-03-12 16:13:08 -0300

    pokemon temos que pegar eu sei

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.Write("Digite o nome do Pokémon: ");
+        string nome = Console.ReadLine().ToLower();
+
+        string url = $"https://pokeapi.co/api/v2/pokemon/{nome}";
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+
+                var pokemon = JsonSerializer.Deserialize<Pokemon>(json);
+
+                Console.WriteLine("\nDados do Pokémon:");
+                Console.WriteLine("Id: " + pokemon.id);
+                Console.WriteLine("Nome: " + pokemon.name);
+                Console.WriteLine("Altura: " + pokemon.height);
+                Console.WriteLine("Peso: " + pokemon.weight);
+            }
+            catch
+            {
+                Console.WriteLine("Pokémon não encontrado.");
+            }
+        }
+    }
+}
